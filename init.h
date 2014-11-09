b30690d976d9b69201c0f553b5d7113a5ea6256a
Niels van Doorn <nielsvandoorn@outlook.com>
2014-11-09 16:54:40 +0100

    Initial commit.
    First essential files setup

---
+++ init.h
@@ -0,0 +1,9 @@
+#ifndef INIT_H
+#define INIT_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+void init();
+
+#endif // INIT_H
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <xc.h>
+#include "main.h"
+#include "init.h"
+
+
+int main() {
+
+    init();
+
+    while (1);
+    return 1;
+}
